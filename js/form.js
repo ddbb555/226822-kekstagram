2202efa20e23b8a76233b3fb5bc343b32c333597
vladimir smirnov <mnblkj567@gmail.com>
2017-02-12 12:44:23 +0300

    Задание 14

--- js/form.js
+++ js/form.js
@@ -2,20 +2,39 @@
 
 var uploadForm = document.querySelector('#upload-select-image'); //выбор фото
 var fotoForm = document.querySelector('.upload-overlay'); // сами фото
-var uploadFile = document.querySelector('#upload-file'); // загрузка
 var fotoFormClose = fotoForm.querySelector('#upload-cancel'); // крестик закрытия
 
-var toggleControlsVisibility = function () {
-    uploadForm.classList.toggle('invisible');
-    fotoForm.classList.toggle('invisible');
-  }
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
 
-function addOpenCloseHandlers() {
-  uploadFile.addEventListener('change', toggleControlsVisibility);
-  fotoFormClose.addEventListener('click',toggleControlsVisibility);
-};
 
-addOpenCloseHandlers();
+function enterPressed(evt) {
+    return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+}
+
+function setupEscEvent(evt) {
+    if (evt.keyCode === ESC_KEY_CODE) {
+        toggleSetup();
+    }
+}
+
+function toggleSetup() {
+    if (fotoForm.classList.contains('invisible')) {
+        fotoForm.classList.remove('invisible');
+        document.addEventListener('keydown', setupEscEvent);
+
+    } else {
+        fotoForm.classList.add('invisible');
+        document.removeEventListener('keydown', setupEscEvent);
+
+    }
+}
+
+
+uploadForm.addEventListener('change', toggleSetup);
+
+fotoFormClose.addEventListener('click', toggleSetup);
+
 
 //Фильтры
 var uploadSection = document.querySelector('.upload');
@@ -27,31 +46,31 @@ var filterPhobos = uploadSection.querySelector('#upload-filter-phobos');
 var filterHeat = uploadSection.querySelector('#upload-filter-heat');
 var imgPreview = uploadSection.querySelector('.filter-image-preview');
 
-var allFilters = ['filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat' ]
 
-filterNone.addEventListener('click', function () {
-  imgPreview.classList.remove(allFilters);
-  imgPreview.classList.add('filter-none');
+
+filterNone.addEventListener('click', function() {
+    imgPreview.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    imgPreview.classList.add('filter-none');
 });
-filterChrome.addEventListener('click', function () {
-  imgPreview.classList.remove(allFilters);
-  imgPreview.classList.add('filter-chrome');
+filterChrome.addEventListener('click', function() {
+    imgPreview.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    imgPreview.classList.add('filter-chrome');
 });
-filterSepia.addEventListener('click', function () {
-  imgPreview.classList.remove(allFilters);
-  imgPreview.classList.add('filter-sepia');
+filterSepia.addEventListener('click', function() {
+    imgPreview.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    imgPreview.classList.add('filter-sepia');
 });
-filterMarvin.addEventListener('click', function () {
-  imgPreview.classList.remove(allFilters);
-  imgPreview.classList.add('filter-marvin');
+filterMarvin.addEventListener('click', function() {
+    imgPreview.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    imgPreview.classList.add('filter-marvin');
 });
-filterPhobos.addEventListener('click', function () {
-  imgPreview.classList.remove(allFilters);
-  imgPreview.classList.add('filter-phobos');
+filterPhobos.addEventListener('click', function() {
+    imgPreview.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    imgPreview.classList.add('filter-phobos');
 });
-filterHeat.addEventListener('click', function () {
-  imgPreview.classList.remove(allFilters);
-  imgPreview.classList.add('filter-heat');
+filterHeat.addEventListener('click', function() {
+    imgPreview.classList.remove('filter-none', 'filter-chrome', 'filter-sepia', 'filter-marvin', 'filter-phobos', 'filter-heat');
+    imgPreview.classList.add('filter-heat');
 });
 
 var resizeValue = fotoForm.querySelector('.upload-resize-controls-value');
@@ -59,24 +78,25 @@ var resizeValueDec = fotoForm.querySelector('.upload-resize-controls-button-dec'
 var resizeValueInc = fotoForm.querySelector('.upload-resize-controls-button-inc');
 
 var resizeNumber = 100;
-resizeValue.value = resizeNumber + '%';
+resizeValue.setAttribute('value', '100%');
 
-var valueScaleChange = function () {
-  var scaleNumber = resizeNumber / 100;
-  resizeValue.value = resizeNumber + '%';
-  imgPreview.style.transform = 'scale(' + scaleNumber + ')';
+
+var valueScaleChange = function() {
+    var scaleNumber = resizeNumber / 100;
+    resizeValue.value = resizeNumber + '%';
+    imgPreview.style.transform = 'scale(' + scaleNumber + ')';
 };
 
-resizeValueDec.addEventListener('click', function () {
-  if (resizeNumber > 25) {
-    resizeNumber = resizeNumber - 25;
-    valueScaleChange();
-  }
+resizeValueDec.addEventListener('click', function() {
+    if (resizeNumber > 25) {
+        resizeNumber = resizeNumber - 25;
+        valueScaleChange();
+    }
 });
 
-resizeValueInc.addEventListener('click', function () {
-  if (resizeNumber < 100) {
-    resizeNumber = resizeNumber + 25;
-    valueScaleChange();
-  }
+resizeValueInc.addEventListener('click', function() {
+    if (resizeNumber < 100) {
+        resizeNumber = resizeNumber + 25;
+        valueScaleChange();
+    }
 });
