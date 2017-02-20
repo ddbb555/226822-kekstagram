de657a263f115c8c1718a64b99a895ce1a485de6
vladimir smirnov <mnblkj567@gmail.com>
2017-02-20 19:20:45 +0300

    Задание 16 #4

--- js/form.js
+++ js/form.js
@@ -1,42 +1,40 @@
 'use strict';
 
-var uploadForm = document.querySelector('#upload-select-image'); //выбор фото
-var fotoForm = document.querySelector('.upload-overlay'); // сами фото
-var fotoFormClose = fotoForm.querySelector('#upload-cancel'); // крестик закрытия
+var uploadForm = document.querySelector('#upload-select-image');
+var fotoForm = document.querySelector('.upload-overlay');
+var fotoFormClose = fotoForm.querySelector('#upload-cancel');
+var uploadFilterControls = document.querySelector('.upload-filter-controls');
+var filterImagePreview = document.querySelector('.filter-image-preview');
 
 var ENTER_KEY_CODE = 13;
 var ESC_KEY_CODE = 27;
 
 function enterPressed(evt) {
-    return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-}
+  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+};
 
 function setupEscEvent(evt) {
-    if (evt.keyCode === ESC_KEY_CODE) {
-        toggleSetup();
+  if (evt.keyCode === ESC_KEY_CODE) {
+      toggleSetup();
     }
-}
+};
 
-var toggleSetup = function() {
-    if (fotoForm.classList.contains('invisible')) {
-        fotoForm.classList.remove('invisible');
-        document.addEventListener('keydown', setupEscEvent);
+var toggleSetup = function () {
+  if (fotoForm.classList.contains('invisible')) {
+      fotoForm.classList.remove('invisible');
+      document.addEventListener('keydown', setupEscEvent);
     } else {
-        fotoForm.classList.add('invisible');
-        document.removeEventListener('keydown', setupEscEvent);
+      fotoForm.classList.add('invisible');
+      document.removeEventListener('keydown', setupEscEvent);
     }
-}
+};
+
+window.enterPressed = enterPressed;
+window.filterImagePreview = filterImagePreview;
 
 uploadForm.addEventListener('change', toggleSetup);
 fotoFormClose.addEventListener('click', toggleSetup);
 
-
-
-
-//------------------------------фильтры---------------------------------------
-
 window.initializeFilters(uploadFilterControls);
-
-//------------------------------Масштаб---------------------------------------
 
 window.initializeScale(document.querySelector('.upload-resize-controls'), 100, 25);
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,28 +1,28 @@
 'use strict';
 
-window.initializeScale = function(element, resizeControlsValueDefault, step) {
-    var resizeValue = element.querySelector('.upload-resize-controls-value');
-    var resizeValueDec = element.querySelector('.upload-resize-controls-button-dec');
-    var resizeValueInc = element.querySelector('.upload-resize-controls-button-inc');
-    resizeValue.setAttribute('value', resizeControlsValueDefault + '%');
-    var resizeNumber = resizeControlsValueDefault;
-    function valueScaleChange(event) {
-        if (event.target.contains(resizeValueDec)) {
-            if (resizeNumber > step) {
-                resizeNumber = resizeNumber - step;
-                var scaleNumber = resizeNumber / resizeControlsValueDefault;
-                resizeValue.value = resizeNumber + '%';
-                filterImagePreview.style.transform = 'scale(' + scaleNumber + ')';;
+window.initializeScale = function (element, resizeControlsValueDefault, step) {
+  var resizeValue = element.querySelector('.upload-resize-controls-value');
+  var resizeValueDec = element.querySelector('.upload-resize-controls-button-dec');
+  var resizeValueInc = element.querySelector('.upload-resize-controls-button-inc');
+  resizeValue.setAttribute('value', resizeControlsValueDefault + '%');
+  var resizeNumber = resizeControlsValueDefault;
+  function valueScaleChange(event) {
+      if (event.target.contains(resizeValueDec)) {
+          if (resizeNumber > step) {
+              resizeNumber = resizeNumber - step;
+              var scaleNumber = resizeNumber / resizeControlsValueDefault;
+              resizeValue.value = resizeNumber + '%';
+              filterImagePreview.style.transform = 'scale(' + scaleNumber + ')';
             }
         }
-        if (event.target.contains(resizeValueInc)) {
-            if (resizeNumber < resizeControlsValueDefault) {
-                resizeNumber = resizeNumber + step;
-                var scaleNumber = resizeNumber / resizeControlsValueDefault;
-                resizeValue.value = resizeNumber + '%';
-                filterImagePreview.style.transform = 'scale(' + scaleNumber + ')';;
+      if (event.target.contains(resizeValueInc)) {
+          if (resizeNumber < resizeControlsValueDefault) {
+              resizeNumber = resizeNumber + step;
+              var scaleNumber = resizeNumber / resizeControlsValueDefault;
+              resizeValue.value = resizeNumber + '%';
+              filterImagePreview.style.transform = 'scale(' + scaleNumber + ')';
             }
         }
     }
-    element.addEventListener('click', valueScaleChange);
+  element.addEventListener('click', valueScaleChange);
 };
