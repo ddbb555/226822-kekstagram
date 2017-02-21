74fc9044da72d7dfa46f24ecf2ed3e4b84cdecf9
vladimir smirnov <mnblkj567@gmail.com>
2017-02-21 20:03:56 +0300

    Задание 18

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,12 +1,14 @@
 'use strict';
 
-window.initializeScale = function (element, resizeControlsValueDefault, step) {
+window.initializeScale = (function () {
+  return function (element, resizeControlsValueDefault, step) {
   var resizeValue = element.querySelector('.upload-resize-controls-value');
   var resizeValueDec = element.querySelector('.upload-resize-controls-button-dec');
   var resizeValueInc = element.querySelector('.upload-resize-controls-button-inc');
   resizeValue.setAttribute('value', resizeControlsValueDefault + '%');
   var resizeNumber = resizeControlsValueDefault;
   var scaleNumber;
+
   function valueScaleChange(event) {
     if (event.target.contains(resizeValueDec)) {
       if (resizeNumber > step) {
@@ -27,3 +29,4 @@ window.initializeScale = function (element, resizeControlsValueDefault, step) {
   }
   element.addEventListener('click', valueScaleChange);
 };
+})();
