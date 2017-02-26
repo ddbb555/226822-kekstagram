cbe7f9393edbfc6846e7d5a15a6146e615d22662
vladimir smirnov <mnblkj567@gmail.com>
2017-02-26 22:05:40 +0300

    задание 24 #8

--- js/pictures.js
+++ js/pictures.js
@@ -19,7 +19,8 @@ window.load('https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/da
 
       function getRandomArrFromArr(arr, n) {
         var shuffleArr = arr.sort(function () {
-        return Math.random() - 0.5 });
+          return Math.random() - 0.5;
+        });
         var elemToSortAmount = n;
         var randomArr = shuffleArr.slice(0, elemToSortAmount);
         return randomArr;
